7f2f5f5f02eedaf5ceeab570706f7aada865619f
Steven Lee <Steven@Blueberry.local>
2018-06-18 11:25:55 -0700

    Add another method of inehirtance

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -4,7 +4,7 @@ var HoneyMakerBee = function() {
 	this.job = 'make honey';
 	this.honeyPot = 0;
 };
-
+//HoneyMakerBee.prototype = Object.create(Bee.prototype);
 HoneyMakerBee.prototype = new Bee();
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
